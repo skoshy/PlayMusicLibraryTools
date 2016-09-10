4d0c2859ca343cf67db204805b8d63d31f76984f
Stefan Koshy <noreply@skoshy.com>
2016-09-09 21:58:16 -0400

    Changed json config to ini, added config option to specify output folder

--- index.js
+++ index.js
@@ -1,83 +1,91 @@
-var PlayMusic = require('playmusic');
+// required
+var pm = new(require('playmusic'));
 var fs = require('fs');
 var dateFormat = require('dateformat');
-var currentDate = new Date();
-var currentDateStr = dateFormat(currentDate, "yyyy-mm-dd-hh-MM-ss");
+var ini = require('ini');
+
+var fileTimestamp = getDate();
+
+var config = ini.parse(fs.readFileSync('config.ini', 'utf8'));
 
 // make the output directory
-fs.mkdir('output', function(err){});
+fs.mkdir(config.outputfolder, function(err){});
 
-var pm = new PlayMusic();
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 console.log('Logging in...');
 pm.init({email: config.username, password: config.password}, function(err) {
-  if (err) console.error(err);
-  console.log('Logged in, getting library...');
-  getLibrary();
+if (err) console.error(err);
+console.log('Logged in, getting library...');
+getLibrary();
 });
 
 function getLibrary() {
-  var allTracks = [];
-  var deletedTracks = [];
-  var trackCount = 0;
-  var page = 1;
-  
-  function getTracks(nextPageToken) {
-    pm.getAllTracks({"nextPageToken": nextPageToken}, function(err, library) {
-      // let's loop through and delete unnecesary data
-      library.data.items.forEach(function(item) {
-        delete item.kind;
-        delete item.clientId;
-        delete item.creationTimestamp;
-        delete item.lastModifiedTimestamp;
-        delete item.recentTimestamp;
-        delete item.durationMillis;
-        delete item.beatsPerMinute;
-        delete item.albumArtRef;
-        delete item.artistArtRef;
-        delete item.totalTrackCount;
-        delete item.discNumber;
-        delete item.totalDiscCount;
-        delete item.year;
-        delete item.rating;
-        delete item.estimatedSize;
-        delete item.trackType;
-        delete item.artistId;
-        delete item.albumId;
-        delete item.nid;
+var allTracks = [];
+var deletedTracks = [];
+var trackCount = 0;
+var page = 1;
 
-        // check if the track has been deleted
-        if (item.deleted) {
-          deletedTracks.push(item);
-        }
-      });
+function getTracks(nextPageToken) {
+pm.getAllTracks({"nextPageToken": nextPageToken}, function(err, library) {
+// let's loop through and delete unnecesary data
+library.data.items.forEach(function(item) {
+delete item.kind;
+delete item.clientId;
+delete item.creationTimestamp;
+delete item.lastModifiedTimestamp;
+delete item.recentTimestamp;
+delete item.durationMillis;
+delete item.beatsPerMinute;
+delete item.albumArtRef;
+delete item.artistArtRef;
+delete item.totalTrackCount;
+delete item.discNumber;
+delete item.totalDiscCount;
+delete item.year;
+delete item.rating;
+delete item.estimatedSize;
+delete item.trackType;
+delete item.artistId;
+delete item.albumId;
+delete item.nid;
 
-      trackCount += library.data.items.length;      
-      allTracks = allTracks.concat(library.data.items);
+// check if the track has been deleted
+if (item.deleted) {
+deletedTracks.push(item);
+}
+});
 
-      if (library.nextPageToken) {
-        page++;
-        console.log('Getting page '+page+'...');
-        getTracks(library.nextPageToken);
-      } else {
-        console.log('Writing to files...');
-        fs.writeFile("output/library."+currentDateStr+".json", JSON.stringify(allTracks, null, 2)+"\n", function() {});
-        fs.writeFile("output/library."+currentDateStr+".meta", "Tracks: "+trackCount+"\n", function() {});
-        fs.writeFile("output/deletedTracks."+currentDateStr+".json", JSON.stringify(deletedTracks, null, 2)+"\n", function() {});
-        fs.writeFile("output/deletedTracks."+currentDateStr+".meta", "Tracks: "+deletedTracks.length+"\n", function() {});
-        console.log('You have '+allTracks.length+' total track(s) in your library');
-        console.log('You have '+deletedTracks.length+' deleted track(s) in your library');
-      }
-    });
-  }
+trackCount += library.data.items.length;      
+allTracks = allTracks.concat(library.data.items);
 
-  console.log('Getting page '+page+'...');
-  getTracks('');
+if (library.nextPageToken) {
+page++;
+console.log('Getting page '+page+'...');
+getTracks(library.nextPageToken);
+} else {
+console.log('Writing to files...');
+fs.writeFile(config.outputfolder+"/library."+fileTimestamp+".json", JSON.stringify(allTracks, null, 2)+"\n", function() {});
+fs.writeFile(config.outputfolder+"/library."+fileTimestamp+".meta", "Tracks: "+trackCount+"\n", function() {});
+fs.writeFile(config.outputfolder+"/deletedTracks."+fileTimestamp+".json", JSON.stringify(deletedTracks, null, 2)+"\n", function() {});
+fs.writeFile(config.outputfolder+"/deletedTracks."+fileTimestamp+".meta", "Tracks: "+deletedTracks.length+"\n", function() {});
+console.log('You have '+allTracks.length+' total track(s) in your library');
+console.log('You have '+deletedTracks.length+' deleted track(s) in your library');
+}
+});
+}
+
+console.log('Getting page '+page+'...');
+getTracks('');
 }
 
 function getLists() {
-  var lists = pm.getPlayLists(function(err, data) {
-    fs.writeFile("output/lists.json", JSON.stringify(data.data), function() {});
-  });
+var lists = pm.getPlayLists(function(err, data) {
+fs.writeFile("output/lists.json", JSON.stringify(data.data), function() {});
+});
+}
+
+function getDate() {	
+	var currentDate = new Date();
+	var currentDateStr = dateFormat(currentDate, "yyyy-mm-dd-hh-MM-ss");
+	return currentDateStr;
 }
